005b205afed5e18c44c54414828d9224d959c98c
hulklee1 <35159494+hulklee1@users.noreply.github.com>
2021-09-24 17:46:51 +0900

    Add files via upload

---
+++ C#/myDB/myDB/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿
+namespace myDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dbGrid = new System.Windows.Forms.DataGridView();
+            this.PopupMenu1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.pmnuColumnAdd = new System.Windows.Forms.ToolStripMenuItem();
+            this.pmnuRowAdd = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuMain = new System.Windows.Forms.MenuStrip();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuMigration = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuColumnAdd = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuRowAdd = new System.Windows.Forms.ToolStripMenuItem();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.PopupMenu2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.test1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.testCMD2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnCmd = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dbGrid)).BeginInit();
+            this.PopupMenu1.SuspendLayout();
+            this.menuMain.SuspendLayout();
+            this.PopupMenu2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dbGrid
+            // 
+            this.dbGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dbGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dbGrid.ContextMenuStrip = this.PopupMenu1;
+            this.dbGrid.Location = new System.Drawing.Point(2, 158);
+            this.dbGrid.Name = "dbGrid";
+            this.dbGrid.RowTemplate.Height = 23;
+            this.dbGrid.Size = new System.Drawing.Size(743, 288);
+            this.dbGrid.TabIndex = 0;
+            // 
+            // PopupMenu1
+            // 
+            this.PopupMenu1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pmnuColumnAdd,
+            this.pmnuRowAdd});
+            this.PopupMenu1.Name = "PopupMenu1";
+            this.PopupMenu1.Size = new System.Drawing.Size(146, 48);
+            // 
+            // pmnuColumnAdd
+            // 
+            this.pmnuColumnAdd.Name = "pmnuColumnAdd";
+            this.pmnuColumnAdd.Size = new System.Drawing.Size(145, 22);
+            this.pmnuColumnAdd.Text = "Column 추가";
+            this.pmnuColumnAdd.Click += new System.EventHandler(this.pmnuColumnAdd_Click);
+            // 
+            // pmnuRowAdd
+            // 
+            this.pmnuRowAdd.Name = "pmnuRowAdd";
+            this.pmnuRowAdd.Size = new System.Drawing.Size(145, 22);
+            this.pmnuRowAdd.Text = "Row 추가";
+            this.pmnuRowAdd.Click += new System.EventHandler(this.pmnuRowAdd_Click);
+            // 
+            // menuMain
+            // 
+            this.menuMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuMain.Location = new System.Drawing.Point(0, 0);
+            this.menuMain.Name = "menuMain";
+            this.menuMain.Size = new System.Drawing.Size(745, 24);
+            this.menuMain.TabIndex = 1;
+            this.menuMain.Text = "menuStrip1";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem1,
+            this.toolStripMenuItem1,
+            this.mnuMigration,
+            this.toolStripSeparator1,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.exitToolStripMenuItem});
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.newToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem1
+            // 
+            this.newToolStripMenuItem1.Name = "newToolStripMenuItem1";
+            this.newToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.newToolStripMenuItem1.Text = "New";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // mnuMigration
+            // 
+            this.mnuMigration.Name = "mnuMigration";
+            this.mnuMigration.Size = new System.Drawing.Size(180, 22);
+            this.mnuMigration.Text = "Migration...";
+            this.mnuMigration.Click += new System.EventHandler(this.mnuMigration_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As...";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(177, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuColumnAdd,
+            this.mnuRowAdd});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // mnuColumnAdd
+            // 
+            this.mnuColumnAdd.Name = "mnuColumnAdd";
+            this.mnuColumnAdd.Size = new System.Drawing.Size(145, 22);
+            this.mnuColumnAdd.Text = "Column 추가";
+            this.mnuColumnAdd.Click += new System.EventHandler(this.mnuColumnAdd_Click);
+            // 
+            // mnuRowAdd
+            // 
+            this.mnuRowAdd.Name = "mnuRowAdd";
+            this.mnuRowAdd.Size = new System.Drawing.Size(145, 22);
+            this.mnuRowAdd.Text = "Row 추가";
+            this.mnuRowAdd.Click += new System.EventHandler(this.mnuRowAdd_Click);
+            // 
+            // tbInput
+            // 
+            this.tbInput.ContextMenuStrip = this.PopupMenu2;
+            this.tbInput.Location = new System.Drawing.Point(85, 56);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(108, 21);
+            this.tbInput.TabIndex = 2;
+            // 
+            // PopupMenu2
+            // 
+            this.PopupMenu2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.test1ToolStripMenuItem,
+            this.testCMD2ToolStripMenuItem});
+            this.PopupMenu2.Name = "PopupMenu2";
+            this.PopupMenu2.Size = new System.Drawing.Size(139, 48);
+            // 
+            // test1ToolStripMenuItem
+            // 
+            this.test1ToolStripMenuItem.Name = "test1ToolStripMenuItem";
+            this.test1ToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.test1ToolStripMenuItem.Text = "Test1";
+            // 
+            // testCMD2ToolStripMenuItem
+            // 
+            this.testCMD2ToolStripMenuItem.Name = "testCMD2ToolStripMenuItem";
+            this.testCMD2ToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.testCMD2ToolStripMenuItem.Text = "Test CMD 2";
+            // 
+            // btnCmd
+            // 
+            this.btnCmd.Location = new System.Drawing.Point(210, 56);
+            this.btnCmd.Name = "btnCmd";
+            this.btnCmd.Size = new System.Drawing.Size(75, 23);
+            this.btnCmd.TabIndex = 3;
+            this.btnCmd.Text = "Go";
+            this.btnCmd.UseVisualStyleBackColor = true;
+            this.btnCmd.Click += new System.EventHandler(this.btnCmd_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 447);
+            this.Controls.Add(this.btnCmd);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.dbGrid);
+            this.Controls.Add(this.menuMain);
+            this.MainMenuStrip = this.menuMain;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dbGrid)).EndInit();
+            this.PopupMenu1.ResumeLayout(false);
+            this.menuMain.ResumeLayout(false);
+            this.menuMain.PerformLayout();
+            this.PopupMenu2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dbGrid;
+        private System.Windows.Forms.MenuStrip menuMain;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem mnuMigration;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Button btnCmd;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mnuColumnAdd;
+        private System.Windows.Forms.ToolStripMenuItem mnuRowAdd;
+        private System.Windows.Forms.ContextMenuStrip PopupMenu1;
+        private System.Windows.Forms.ToolStripMenuItem pmnuColumnAdd;
+        private System.Windows.Forms.ToolStripMenuItem pmnuRowAdd;
+        private System.Windows.Forms.ContextMenuStrip PopupMenu2;
+        private System.Windows.Forms.ToolStripMenuItem test1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem testCMD2ToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ C#/myDB/myDB/Form1.cs
@@ -0,0 +1,92 @@
+﻿using myLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace myDB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        iniFile ini = new iniFile(".\\myDB.ini");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int x = int.Parse(ini.GetString("Position", "LocationX", "0"));
+            int y = int.Parse(ini.GetString("Position", "LocationY", "0"));
+            Location = new Point(x, y);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ini.WriteString("Position", "LocationX", $"{Location.X}");
+            ini.WriteString("Position", "LocationY", $"{Location.Y}");
+        }
+
+        private void btnCmd_Click(object sender, EventArgs e)
+        {
+            dbGrid.Columns.Add(tbInput.Text, tbInput.Text);
+        }
+
+        private void mnuColumnAdd_Click(object sender, EventArgs e)
+        {
+            string str = mylib.GetInput("컬럼명");
+            dbGrid.Columns.Add(str, str);
+        }
+
+        private void pmnuColumnAdd_Click(object sender, EventArgs e)
+        {
+            mnuColumnAdd_Click(sender, e);
+            //string str = mylib.GetInput("컬럼명");
+            //dbGrid.Columns.Add(str, str);
+        }
+
+        private void mnuRowAdd_Click(object sender, EventArgs e)
+        {
+            if(dbGrid.ColumnCount > 0)
+                dbGrid.Rows.Add();  // 1 Line 추가
+            dbGrid.Rows[1].Cells[5].Value = "";
+        }
+
+        private void pmnuRowAdd_Click(object sender, EventArgs e)
+        {
+            mnuRowAdd_Click(sender, e);
+        }
+
+        private void mnuMigration_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.Cancel) return;
+
+            dbGrid.Rows.Clear();
+            dbGrid.Columns.Clear();
+            StreamReader sr = new StreamReader(openFileDialog.FileName);
+            string buf = sr.ReadLine();
+            string[] sArr = buf.Split(',');
+            for(int i=0; i<sArr.Length;i++)
+            {
+                dbGrid.Columns.Add(sArr[i], sArr[i]);
+            }
+            while(true)
+            {
+                buf = sr.ReadLine();
+                if (buf == null) break;
+                sArr = buf.Split(',');
+                dbGrid.Rows.Add(sArr);
+                //int rIdx = dbGrid.Rows.Add();
+                //for (int i = 0; i < sArr.Length; i++)
+                //{
+                //    dbGrid.Rows[rIdx].Cells[i].Value = sArr[i];
+                //}
+            }
+        }
+    }
+}
